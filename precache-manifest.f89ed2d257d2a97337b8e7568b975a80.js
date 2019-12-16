f92fcbf9340a68fdc30c744562652374ce842b78
Ameya Joshi <ameya.joshi@ruralsource.com>
2019-12-16 09:29:40 -0600

    Updates

---
+++ precache-manifest.f89ed2d257d2a97337b8e7568b975a80.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d6d6dcf33d278fe304bc126a102237b4",
+    "url": "/CoderAJ7470/HN2GO/index.html"
+  },
+  {
+    "revision": "2722c2aedc8a3fccaad4",
+    "url": "/CoderAJ7470/HN2GO/static/css/main.8edfb978.chunk.css"
+  },
+  {
+    "revision": "576ed5b710bdcc470072",
+    "url": "/CoderAJ7470/HN2GO/static/js/2.8ae34d9a.chunk.js"
+  },
+  {
+    "revision": "2722c2aedc8a3fccaad4",
+    "url": "/CoderAJ7470/HN2GO/static/js/main.6f64f647.chunk.js"
+  },
+  {
+    "revision": "01e64c189ef93c5e6c7a",
+    "url": "/CoderAJ7470/HN2GO/static/js/runtime-main.4375dd14.js"
+  }
+]);
